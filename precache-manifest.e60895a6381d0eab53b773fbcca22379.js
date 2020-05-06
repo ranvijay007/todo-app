e9b40541fe9fb041ebcba614a3e717af2d0b5456
Ranvijay_Singh <vijaypop12@gmail.com>
2020-05-06 13:10:50 -0700

    Updates

---
+++ precache-manifest.e60895a6381d0eab53b773fbcca22379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e629036bcccba03e4fc1e839778aaa0d",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "377c4d00d644f6f3f197",
+    "url": "/todo-app/static/css/main.66a5b56e.chunk.css"
+  },
+  {
+    "revision": "490ddb8ec164647ec00a",
+    "url": "/todo-app/static/js/2.2b757a83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.2b757a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377c4d00d644f6f3f197",
+    "url": "/todo-app/static/js/main.52538fac.chunk.js"
+  },
+  {
+    "revision": "2986ea79fb7a68355c87",
+    "url": "/todo-app/static/js/runtime-main.a8e7edd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
